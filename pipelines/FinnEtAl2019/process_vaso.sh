cc10020371a162235906c5c108c29e4dd7d3f0f8
Denis Chaimow <dchaimow@users.noreply.github.com>
2021-05-04 21:46:36 +0200

    continued finnetal vaso pipeline

--- pipelines/FinnEtAl2019/process_vaso.sh
+++ pipelines/FinnEtAl2019/process_vaso.sh
@@ -7,41 +7,38 @@ dataDir=$2
 TR=4.012
 shiftFraction=0.5
 
-
-
 # import
 importruns_vaso-split_reverse.sh \
-    func ${dataDir}/${subj}/func/${subj}_task-layer_run-?_bold.nii.gz
+    func ${TR} ${dataDir}/${subj}/func/${subj}_task-layer_run-?_bold.nii.gz
 
 # find out which tasks
 find_task-runs.sh \
-    func  ${dataDir}/${subj}/func/${subj}_task-layer_run-?_bold.nii.gz
+    func ${TR} ${dataDir}/${subj}/func/${subj}_task-layer_run-?_bold.nii.gz
 
 # motioncorrect
 motioncorrect_vaso.sh $(< func_runs_basenames.txt)
 
-exit
-
-# TODO: make sure to use the motion corrected runs
 # avgruns according to conditions
-avgruns func_alpha-rem_nulled.nii \
-        $(select_runs_add-ext.sh func_runs_basename.txt nulled.nii \
+avgruns.sh func_alpha-rem_nulled.nii \
+        $(select_runs_add-ext.sh func_runs_basenames.txt _nulled_mc.nii \
                                  $(< func_alpha-rem_task-runs.txt))
-avgruns func_alpha-rem_notnulled.nii \
-        $(select_runs_add-ext.sh func_runs_basename.txt notnulled.nii \
+avgruns.sh func_alpha-rem_notnulled.nii \
+        $(select_runs_add-ext.sh func_runs_basenames.txt _notnulled_mc.nii \
                                  $(< func_alpha-rem_task-runs.txt))
-avgruns func_go-nogo_nulled.nii \
-        $(select_runs_add-ext.sh func_runs_basename.txt nulled.nii \
+avgruns.sh func_go-nogo_nulled.nii \
+        $(select_runs_add-ext.sh func_runs_basenames.txt _nulled_mc.nii \
                                  $(< func_go-nogo_task-runs.txt))
-avgruns func_go-nogo_notnulled.nii \
-        $(select_runs_add-ext.sh func_runs_basename.txt notnulled.nii \
+avgruns.sh func_go-nogo_notnulled.nii \
+        $(select_runs_add-ext.sh func_runs_basenames.txt _notnulled_mc.nii \
                                  $(< func_go-nogo_task-runs.txt))
 
-# average all
-avgruns func_all_nulled.nii func?_mc
-        
+# average all (only for t1 calculation)
+avgruns.sh func_all_nulled.nii func?_nulled_mc.nii
+avgruns.sh func_all_notnulled.nii func?_notnulled_mc.nii
 
 # calct1
+calct1.sh func_all
 
 # bold correction
-
+boldcorect.sh func_alpha-rem ${shiftFraction}
+boldcorect.sh func_go-nogo ${shiftFraction}
