2491d270e947a5cc8ca94a861866aafabf19b6a9
Denis Chaimow <dchaimow@users.noreply.github.com>
2021-06-02 11:20:48 +0200

    vaso processing rename nonnulled to bold

--- pipelines/FinnEtAl2019/process_vaso.sh
+++ pipelines/FinnEtAl2019/process_vaso.sh
@@ -46,6 +46,70 @@ calct1.sh func_all
 
 # bold correction
 boldcorrect.sh func_alpha-rem ${shiftFraction}
+mv func_alpha-rem_notnulled.nii func_alpha-rem_bold.nii
 boldcorrect.sh func_go-nogo ${shiftFraction}
+mv func_go-nogo_notnulled.nii func_go-nogo_bold.nii
+
+# # trial averaging
+# trialOrder="1 0 0 0 1 1 0 1 0 0 1 1 1 0 1 0 0 0 1 1"
+# onsetDelay_s=4 # stim starts 4s after this 4s delay
+# trialDuration_s=32
+# onsetTimes_A=$(python ../../code/calc_onset-times.py \
+#                       0 ${onsetDelay_s} ${trialDuration} ${trialOrder})
+# onsetTimes_B=$(python ../../code/calc_onset-times.py \
+#                       1 ${onsetDelay_s} ${trialDuration} ${trialOrder})
+# trialAvgDuration=44
+# trial_dt=4
+# for modality in bold vaso
+# do
+#     avgtrials.sh func_go-nogo_${modality}.nii trials_go_${modality}.nii \
+#              $trialAvgDuration $trial_dt \
+#              $onsetTimes_A
+#     avgtrials.sh func_go-nogo_${modality}.nii trials_nogo_${modality}.nii \
+#              $trialAvgDuration $trial_dt \
+#              $onsetTimes_B
+
+#     avgtrials.sh func_alpha-rem_${modality}.nii trials_alpha_${modality}.nii \
+#              $trialAvgDuration $trial_dt \
+#              $onsetTimes_A
+#     avgtrials.sh func_alpha-rem_${modality}.nii trials_rem_${modality}.nii \
+#              $trialAvgDuration $trial_dt \
+#              $onsetTimes_B
+# done
+
+
+# # normalization
+# for condition in go nogo alpha rem
+# do
+#     for modality in bold vaso
+#     do
+#         3dcalc -a trials_${condition}_${modality}.nii \
+#                -b trials_${condition}_${modality}.nii'[0]' \
+#                -expr '100*a/b - 100' \
+#                -prefix trials_rchg_${condition}_${modality}.nii -overwrite
+#     done
+# done
+
+# label=0002
+# for layer in superficial deeper
+# do
+#     3dcalc -a lh.zstat1_smooth_clusters.label-${label}_in-func.nii \
+#            -b ${layer}.nii \
+#            -expr 'and(a,b)' \
+#            -prefix layermask.nii -overwrite
+#     for condition in go nogo alpha rem
+#     do
+#         for modality in bold vaso
+#         do
+#             3dmaskave -quiet \
+#                       -mask layermask.nii \
+#                       trials_rchg_${condition}_${modality}.nii \
+#                       > ${modality}_${layer}_${condition}.1D
+#         done
+#     done
+# done
+
+# # visualize
+# python3 ../../code/visualize_single-subject.py 
 
 cd ${curDir}
