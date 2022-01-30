2ccc9cf5bf24c49e854b30bc0d6a4b7927dd364c
Denis Chaimow <dchaimow@users.noreply.github.com>
2022-01-30 19:39:34 +0100

    changed bias field correction in calct1 to use spm with clipping

--- library/calct1.sh
+++ library/calct1.sh
@@ -25,7 +25,20 @@ rm ${fBaseName}_combined.nii
 LN_MP2RAGE_DNOISE -INV1 ${fBaseName}_mean_nulled.nii -INV2 ${fBaseName}_mean_nulled.nii \
                   -UNI ${fBaseName}_T1_raw.nii -beta 5 -output ${fBaseName}_T1_denoised.nii
 
-N4BiasFieldCorrection -i ${fBaseName}_T1_denoised.nii -o ${fBaseName}_T1.nii
+# clip 1 - make bias field correction less dependent on few extreme voxels
+3dcalc -datum short -nscale -a ${fBaseName}_T1_denoised.nii \
+       -expr "32767*max(0,min(1,a/(10*$(3dClipLevel ${fBaseName}_T1_denoised.nii))))" \
+       -prefix ${fBaseName}_T1_denoised.nii -overwrite
+                                  
+# bias field correction
+spm_bias-correct.py ${fBaseName}_T1_denoised.nii
+mv m${fBaseName}_T1_denoised.nii ${fBaseName}_T1.nii
+#N4BiasFieldCorrection -i ${fBaseName}_T1_denoised.nii -o ${fBaseName}_T1.ni
+
+# clip 2 - clip values that exceed expected distribution of intensities
+3dcalc -datum byte -nscale -a ${fBaseName}_T1.nii \
+       -expr "255*max(0,min(1,a/(4.2*$(3dClipLevel ${fBaseName}_T1.nii))))" \
+       -prefix ${fBaseName}_T1.nii -overwrite
 
 # calculate mask from notnulled
 export FSLOUTPUTTYPE=NIFTI
