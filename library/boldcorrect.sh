13dd6b42f080a35c1f971612d28458eb737c0110
Denis Chaimow <dchaimow@users.noreply.github.com>
2024-10-04 23:19:36 +0200

    comment edits

--- library/boldcorrect.sh
+++ library/boldcorrect.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-# boldcorrect.sh <basename> <TR> <shiftFraction>
+# boldcorrect.sh <basename> <shiftFraction>
 #
 # - does bold correction of VASO by dividing nulled volumes by time shifted notnulled volumes
-# - shiftFraction is shift relative to TR, positive value shifts back in time
+# - shiftFraction is shift relative to TR (usually a positive number)
 
 fBaseName=$1
 shiftFraction=$2
@@ -13,6 +13,7 @@ export FSLOUTPUTTYPE=NIFTI
 # shift notnulled (BOLD)
 slicetimer -i ${fBaseName}_notnulled -o ${fBaseName}_notnulled_tshift.nii \
            --tglobal=-${shiftFraction}
+
 # BOLD correction
 fslmaths  ${fBaseName}_nulled -div ${fBaseName}_notnulled_tshift.nii \
           -max 0 -min 5 ${fBaseName}_vaso.nii
